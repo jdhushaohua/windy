dca4816405bce3291c160a800dcd519070016ba5
jdhushaohua <hushaohua@jd.com>
2017-06-12 15:21:34 +0800

    去掉无用的代码

--- testGit/src/com/git/Demo.java
+++ testGit/src/com/git/Demo.java
@@ -3,8 +3,5 @@ package com.git;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello git!");
-		System.out.println("windy");//commit;
-		System.out.println("dfasd");
-		System.out.println("windy");
 	}
 }
