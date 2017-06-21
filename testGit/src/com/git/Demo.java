ee8797ca1b8a9416c39acf6c3d2be9497dadf809
jdhushaohua <hushaohua@jd.com>
2017-06-21 16:05:10 +0800

    带伞发送的发生的

--- testGit/src/com/git/Demo.java
+++ testGit/src/com/git/Demo.java
@@ -3,6 +3,6 @@ package com.git;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello git!");
-		System.out.println("sdfasdf");
+		System.out.println("testBranch0621");
 	}
 }
